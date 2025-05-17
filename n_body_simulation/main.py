6f79a488273eea4a985f177f3042977bd82084a0
Adam <adam.perrott@protonmail.com>
2025-05-17 12:42:53 +0100

    Add class methods to find distance and force for each instance

--- n_body_simulation/main.py
+++ n_body_simulation/main.py
@@ -1,6 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy as sp
+import math
+from typing import List
+
+"""
+Structure of dictionary to add bodies:
+'x': ...
+'y': ...
+'vx': ...
+'vy': ...
+'mass': ...
+"""
 
 def csv_to_listofdicts( path: str ):
     df = pd.read_csv(f'{path}', comment='#', header=None, names=['x', 'y', 'vx', 'vy', 'mass'])
@@ -15,15 +27,42 @@ def get_time_constraint() -> tuple:
 
 class Bodies:
 
-    def __init__( self, data: dict ):
+    G = 6.67430e-11
+
+
+    def __init__( self, data: dict, name: str ) -> None:
         self.x = data['x']
         self.y = data['y']
         self.vx = data['vx']
         self.vy = data['vy']
         self.mass = data['mass']
+        self.name = name
 
-    def __repr__( self ):
+    def __repr__( self ) -> str:
         return f"Body (x={self.x}, y={self.y}, vx={self.vx}, vy={self.vy}, mass={self.mass})"
+    
+    @classmethod
+    def dist( self, other: "Bodies" ) -> tuple[ float, float, float ]:
+        dx = other.x - self.x
+        dy = other.y - self.y
+        return math.sqrt( dx ** 2 + dy ** 2), dx, dy
+    
+    @classmethod
+    def net_grav_force( self, others: List[ "Bodies" ] ) -> tuple[ float, float, float ]:
+        fx_total = 0.0
+        fy_total = 0.0
+        for other in others:
+            if other is self:
+                continue
+            r, dx, dy = self.dist( other )
+            if r == 0:
+                continue
+            force_mag = Bodies.G * self.mass * other.mass / r ** 2
+            fx_total += force_mag * dx / r
+            fy_total += force_mag * dy / r
+            return force_mag, fx_total, fy_total
+
+
 
 
 
@@ -40,8 +79,5 @@ def create_list_of_dt ( input: tuple ) -> np.ndarray:
     else:
         return timesteps[timesteps <= input[1]]
     
-
-
-list_of_instances = initialise_many_bodies(csv_to_listofdicts('D:\\computational_physics\\n_body_simulation\\bodies.csv'))
 
 

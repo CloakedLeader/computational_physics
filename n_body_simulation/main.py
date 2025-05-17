cbbe3a375b0808f1324944e137f232cdc47febdf
Adam <adam.perrott@protonmail.com>
2025-05-17 13:43:24 +0100

    Add force vector and force magnitude methods.

--- n_body_simulation/main.py
+++ n_body_simulation/main.py
@@ -29,6 +29,8 @@ class Bodies:
 
     G = 6.67430e-11
 
+    _instances = []
+
 
     def __init__( self, data: dict, name: str ) -> None:
         self.x = data['x']
@@ -37,17 +39,20 @@ class Bodies:
         self.vy = data['vy']
         self.mass = data['mass']
         self.name = name
+        Bodies._instances.append(self)
 
     def __repr__( self ) -> str:
         return f"Body (x={self.x}, y={self.y}, vx={self.vx}, vy={self.vy}, mass={self.mass})"
     
-    @classmethod
+    def all_instances(cls):
+        return cls._instances
+    
     def dist( self, other: "Bodies" ) -> tuple[ float, float, float ]:
         dx = other.x - self.x
         dy = other.y - self.y
         return math.sqrt( dx ** 2 + dy ** 2), dx, dy
     
-    @classmethod
+    
     def net_grav_force( self, others: List[ "Bodies" ] ) -> tuple[ float, float, float ]:
         fx_total = 0.0
         fy_total = 0.0
@@ -60,15 +65,29 @@ class Bodies:
             force_mag = Bodies.G * self.mass * other.mass / r ** 2
             fx_total += force_mag * dx / r
             fy_total += force_mag * dy / r
+            self.force_vec = (fx_total, fy_total)
+            self.force_mag = force_mag
             return force_mag, fx_total, fy_total
+    
+    
+    def net_force_vec( self, others: List[ "Bodies" ] ) -> tuple[ float, float ]:
+        self.force_vec = self.net_grav_force( others )[1:]
+        return self.force_vec
+    
+    
+    def net_force_mag( self, others: List[ "Bodies" ] ) -> float:
+        self.force_mag = self.net_grav_force( others )[0]
+        return self.force_mag 
+    
+    def accel( self ) -> tuple[ float, float ]:
+        pass
 
 
 
-
-
-def initialise_many_bodies( input: list ) -> None:
+def initialise_many_bodies( input: list ) -> List:
     body_dict = []
     for i in input:
+        input()
         body_dict.append(Bodies(i))
     return body_dict
 

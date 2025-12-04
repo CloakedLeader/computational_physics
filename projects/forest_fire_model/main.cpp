55f4f77aeabe3e4775b1110105569691b3513a50
Adam <adam.perrott@protonmail.com>
2025-12-04 15:30:55 +0000

    feat: change timesteps to not update all points at once

--- projects/forest_fire_model/main.cpp
+++ projects/forest_fire_model/main.cpp
@@ -7,6 +7,8 @@
 #include <algorithm>
 #include <array>
 
+/* TODO: Implement an Enum instead of numbers for the classification of points on the lattice.*/
+
 double random_num() {
     static std::mt19937 rng(std::random_device{}());
     static std::uniform_real_distribution<double> dist(0.0, 1.0);
@@ -19,15 +21,17 @@ private:
     double fire_prob;
     double grow_prob;
     int steps;
+    int subLatticeSize;
 
     std::vector<std::vector<int>> current_lattice;
     std::vector<std::vector<int>> next_lattice;
 
-    struct {
+    struct MetricsData{
         int newIgnitions = 0;
         int totalBurned = 0;
-    } Metrics;
+    };
 
+    MetricsData Metrics;
 
 public:
     ForestLattice(int size_, int steps_, double f, double p)
@@ -36,6 +40,7 @@ public:
     {
         current_lattice.resize(size, std::vector<int>(size, 0));
         next_lattice.resize(size, std::vector<int>(size, 0));
+        subLatticeSize = static_cast<int>(size * 0.1);
     }
 
     void lightNeighbourTrees(int x, int y) {
@@ -45,16 +50,50 @@ public:
             int nx = x + xOffsets[k];
             int ny = y + yOffsets[k];
             if (current_lattice[nx][ny] == 1 &&
-                0 <= nx < size && 0 <= ny < size) {
+                0 <= nx && nx < size && 0 <= ny && ny < size) {
                     next_lattice[nx][ny] = 2;
                 }
         }
 
     }
 
-    void advanceOneStep() {
+    std::array<std::vector<int>, 2> chooseRandomPoints () {
+        std::vector<int> xs(subLatticeSize);
+        std::vector<int> ys(subLatticeSize);
+        for (int i = 0; i < subLatticeSize; i++) {
+            xs[i] = static_cast<int>(random_num() * (subLatticeSize - 1));
+            ys[i] = static_cast<int>(random_num() * (subLatticeSize - 1));
+        }
 
-        next_lattice = current_lattice;
+        return {xs, ys};
+    }
+
+    void advanceOneStep() {
+        std::swap(current_lattice, next_lattice);
+        std::array<std::vector<int>, 2> randomPoints = chooseRandomPoints();
+        for (int i = 0; i < subLatticeSize; i++) {
+            int coords[2] = {randomPoints[0][i], randomPoints[1][i]};
+            if (current_lattice[coords[0]][coords[1]] == 0) {
+                    if (random_num() < grow_prob) {
+                        next_lattice[coords[0]][coords[1]] = 1;
+                    }
+                }
+                else if (current_lattice[coords[0]][coords[1]] == 1) {
+                    if (random_num() < fire_prob) {
+                        Metrics.newIgnitions++;
+                        next_lattice[coords[0]][coords[1]] = 2;
+                    }
+                }
+                else if (current_lattice[coords[0]][coords[1]] == 2) {
+                    lightNeighbourTrees(coords[0], coords[1]);
+                    Metrics.totalBurned++;
+                    next_lattice[coords[0]][coords[1]] = 0;
+
+                }
+
+        }
+        
+        /*
         for (int i = 0; i < size; i++) {
             for (int j = 0; j< size; j++) {
                 if (current_lattice[i][j] == 0) {
@@ -76,6 +115,7 @@ public:
                 }
             }
         }
+        */
     }
 
     std::array<int, 3> countInstMetrics () {
@@ -105,7 +145,6 @@ public:
             std::array<int, 3> inst = countInstMetrics();
             file << t << "," << inst[0] << "," << inst[1] << "," << inst[2] << '\n';
         }
-        file.close();
         std::cout << "Data written to results.csv" << '\n';
     }
 };
